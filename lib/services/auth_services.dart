72489c6556a0292ca41f0f92d22c55dc18086fbd
Vicky8061 <vickykhalasi0318@gmail.com>
2026-01-12 14:34:33 +0530

    ip changed

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -6,7 +6,7 @@ import 'package:userdata/model/auth_request.dart';
 class AuthServices {
   static Future<Map<String, dynamic>> login(AuthRequest request) async {
     final response = await http.post(
-      Uri.parse("http://152.59.33.99:3000/flutter_api/register.php"),
+      Uri.parse("http://10.218.201.253:3000/flutter_api/register.php"),
       headers: {"Content-Type": "application/json"},
       body: request.toLoginMap(),
     );
@@ -15,7 +15,7 @@ class AuthServices {
 
   static Future<Map<String, dynamic>> register(AuthRequest request) async {
     final response = await http.post(
-      Uri.parse("http://152.59.33.99:3000/flutter_api/login.php"),
+      Uri.parse("http://10.218.201.253:3000/flutter_api/login.php"),
       headers: {"Content-Type": "application/json"},
       body: request.toRegisterMap(),
     );
